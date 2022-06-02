473eb13c10a6eb60a2a53b5223837a163aa51c43
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2022-06-02 10:06:17 +0800

    2nd commit comment

--- finalapp/views.py
+++ finalapp/views.py
@@ -6,10 +6,13 @@ from .models import *
 from django.contrib import messages
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+
+#base function
 def base_view(request):
     # return response
     return render(request, "base.html")
 
+#search view
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
@@ -18,11 +21,9 @@ def search(request):
     else:
         return render(request, 'search.html', {})
 
-
-
-
+#index view
 def index(request):
-    customer_list = Customers.objects.all()
+    customer_list = Customers.objects.all().order_by('date')
     page = request.GET.get('page', 1)
     paginator = Paginator(customer_list,5)
     try:
@@ -37,17 +38,7 @@ def index(request):
     }
     return render(request,'index.html', context)
 
-
-#def index(request):
-    #return HttpResponse("This is the index page.")
-#    customer_list = Customers.objects.all()
-#    p = Paginator(Customers.objects.all(), 5)
-#    page = p.page(2)
-#    context = {
-#        'customer_list' : customer_list
-#    }
-#    return render(request,'index.html', context)
-
+#add new records function
 def add(request):  
     if request.method=="POST":
         name = request.POST['name']
@@ -69,13 +60,14 @@ def add(request):
 
     return render(request,'add.html', context)
 
-
+#delete record function
 def delete(request, myid):
     customer = Customers.objects.get(id = myid)
     customer.delete()
     messages.info(request, "Customer deleted successfully!")
     return redirect(index)
 
+#edit record function
 def edit(request, myid):
     edit_customer = Customers.objects.get(id = myid)
     customer_list = Customers.objects.all()
@@ -85,6 +77,7 @@ def edit(request, myid):
     }
     return render(request, 'edit.html', context)
 
+#update record function
 def update(request, myid):
     customer = Customers.objects.get(id = myid)
     customer.name = request.POST['name']
@@ -97,6 +90,7 @@ def update(request, myid):
     messages.info(request,"Customer updated successfully!")
     return redirect('index')
 
+#view function
 def view(request, myid):
     view_customer = Customers.objects.get(id = myid)
     edit_customer = Customers.objects.all()
@@ -108,5 +102,6 @@ def view(request, myid):
     }
     return render(request, 'view.html', context)
 
+#return to index function when user view a record
 def back(request):
     return redirect('index')
